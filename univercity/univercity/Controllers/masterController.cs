4b150182ce3dd3a136ef768f6dca60fa4a680295
masoomeshams <fmezadi9304265430@gmail.com>
2019-06-12 14:15:54 -0700

    اضافه کردن کنترلر مستر

---
+++ univercity/univercity/Controllers/masterController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace univercity.Controllers
+{
+    public class masterController : Controller
+    {
+        // GET: master
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
