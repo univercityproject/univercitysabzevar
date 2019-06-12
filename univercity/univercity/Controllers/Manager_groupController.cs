61a87037daa9aa4927353302f6e0836e37a1b3a2
masoomeshams <fmezadi9304265430@gmail.com>
2019-06-12 16:24:32 -0700

    ساخت کنترلرها ساخت layiout

---
+++ univercity/univercity/Controllers/Manager_groupController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace univercity.Controllers
+{
+    public class Manager_groupController : Controller
+    {
+        // GET: Manager_group
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- univercity/univercity/Controllers/masterController.cs
+++ univercity/univercity/Controllers/masterController.cs
@@ -3,15 +3,60 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using univercity.Models;
 
 namespace univercity.Controllers
 {
     public class masterController : Controller
     {
+        univercityEntities db = new univercityEntities();
         // GET: master
-        public ActionResult Index()
+        public ActionResult home()
         {
             return View();
         }
+        public ActionResult login()
+        {
+            TempData["msg"] = "";
+            return View();
+        }
+
+        public ActionResult _login(string txt_usernam ,string txt_pass)
+        {
+            //مدیراصلی
+
+            tbl_adminstrator tba = db.tbl_adminstrator.Where(a=> a.name_user==txt_usernam && a.password==txt_pass).SingleOrDefault();
+            if (tba.code_national_teacher==null)
+            {
+                TempData["msg"] = "نام کاربری یا رمز یافت نشد";
+                return View("login");
+            }
+            else
+            {
+                return RedirectToAction("home","Admin");
+
+            }
+        }
+
+        public ActionResult show_list()
+        {
+            return View();
+        }
+
+        public ActionResult show_project()
+        {
+            return View();
+        }
+
+        public ActionResult show_offer()
+        {
+            return View();
+        }
+
+        public ActionResult contact_us()
+        {
+            return View();
+        }
+
     }
 }
